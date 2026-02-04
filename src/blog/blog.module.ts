32579f723c86eb19c84a93a2ab808510c1ccecbd
Ahmad <ahmadasgf89@gmail.com>
2026-02-04 18:06:10 +0700

    chore: make db module to global module

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { BlogService } from './blog.service';
 import { BlogController } from './blog.controller';
 import { BlogRepository } from './blog.repository';
-import { DatabaseService } from '../database/database.service';
 
 @Module({
   controllers: [BlogController],
-  providers: [BlogService, BlogRepository, DatabaseService],
+  providers: [BlogService, BlogRepository],
 })
 export class BlogModule {}
