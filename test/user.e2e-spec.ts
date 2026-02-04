14c6ee1edc00de12845aafcb36a11b7f445381d9
Ahmad <ahmadasgf89@gmail.com>
2026-02-04 23:20:23 +0700

    chore: add user e2e test

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -6,7 +6,7 @@ import { AuthResponse } from 'src/auth/jwt.interface';
 
 const versions = ['v1'];
 
-describe.each(versions)('Auth E2E - login | Register -> refresh', (version) => {
+describe.each(versions)('User E2E - login -> profile ', (version) => {
   let app: INestApplication;
   let accessToken: string;
   let refreshToken: string;
@@ -45,40 +45,12 @@ describe.each(versions)('Auth E2E - login | Register -> refresh', (version) => {
     expect(refreshToken).toBeDefined();
   });
 
-  it('POST /api/v1/auth/reqister -> success', async () => {
+  it('GET /api/user/me', async () => {
     const res = await request(app.getHttpServer())
-      .post(`/api/${version}/auth/reqister`)
-      .send({
-        username: `e2e_${version}_${Date.now()}`,
-        password: 'password',
-      });
-
-    const body = res.body as AuthResponse;
-
-    expect(res.status).toBe(201);
-    expect(res.body).toBeDefined();
-
-    accessToken = body.tokens.accessToken;
-    refreshToken = body.tokens.refreshToken;
-
-    expect(accessToken).toBeDefined();
-    expect(refreshToken).toBeDefined();
-  });
-
-  it('GET /api/v1/auth/refresh -> success', async () => {
-    const res = await request(app.getHttpServer())
-      .post(`/api/${version}/auth/refresh`)
-      .set('Authorization', `Bearer ${refreshToken}`);
+      .get(`/api/${version}/user/me`)
+      .set('Authorization', `Bearer ${accessToken}`);
 
     expect(res.status).toBe(200);
     expect(res.body).toBeDefined();
-
-    const body = res.body as AuthResponse;
-
-    accessToken = body.tokens.accessToken;
-    refreshToken = body.tokens.refreshToken;
-
-    expect(accessToken).toBeDefined();
-    expect(refreshToken).toBeDefined();
   });
 });
