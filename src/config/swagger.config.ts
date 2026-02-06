e52d31e147e400b0f6a49e8053fd873fba8d4835
Ahmad <ahmadasgf89@gmail.com>
2026-02-06 21:43:04 +0700

    chore: update swagger config

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,22 +1,28 @@
 import { registerAs } from '@nestjs/config';
 import * as Joi from 'joi';
 
-export const SWAGGER_CONFIG_NAME_KEY = 'swagger' as const;
+export const CONFIG_NAME = 'swagger' as const;
 
-export interface SwaggerConfigI {
-  SWAGGER_DOCS_URL: string;
-}
+export const swaggerConfig = registerAs(CONFIG_NAME, () => ({
+  SWAGGER_DOCS_URL: process.env.SWAGGER_DOCS_URL!,
+}));
+
+type SwaggerConfigs = ReturnType<typeof swaggerConfig>;
+type SwaggerKey = keyof SwaggerConfigs;
 
 export const swaggerValidationSchema = {
   SWAGGER_DOCS_URL: Joi.string().required(),
 };
 
-export default registerAs<SwaggerConfigI>(
-  SWAGGER_CONFIG_NAME_KEY,
-  (): SwaggerConfigI => ({
-    SWAGGER_DOCS_URL: process.env.SWAGGER_DOCS_URL!,
-  }),
-);
+export const CONSTANTS_SWAGGER_KEYS: {
+  SWAGGER: {
+    [K in SwaggerKey]: `${typeof CONFIG_NAME}.${K}`;
+  };
+} = {
+  SWAGGER: {
+    SWAGGER_DOCS_URL: 'swagger.SWAGGER_DOCS_URL',
+  },
+} as const;
 
 export interface SwaggerConfig {
   title: string;
@@ -33,14 +39,4 @@ export const SWAGGER_CONFIG: SwaggerConfig = {
   tags: [],
 };
 
-type JwtKey = keyof SwaggerConfigI;
-
-export const CONSTANTS_SWAGGER_KEYS: {
-  SWAGGER: {
-    [K in JwtKey]: `${typeof SWAGGER_CONFIG_NAME_KEY}.${K}`;
-  };
-} = {
-  SWAGGER: {
-    SWAGGER_DOCS_URL: 'swagger.SWAGGER_DOCS_URL',
-  },
-} as const;
+export default swaggerConfig;
