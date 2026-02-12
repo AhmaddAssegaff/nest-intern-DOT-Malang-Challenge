cb7e68fdfd48b9d0462bc98a7422381ceadcd4e8
Ahmad <ahmadasgf89@gmail.com>
2026-02-12 10:34:44 +0700

    chore: rename file config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { BlogModule } from './blog/blog.module';
 import { DatabaseModule } from './database/database.module';
-import { ConfigurationModule } from './configurationModule.module';
+import { ConfigurationModule } from './configuration.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
